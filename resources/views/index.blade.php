c4c452cbb62293f642771d990bb003458db2f141
vagrant <vagrant@homestead>
2023-07-06 10:14:56 +0000

    Add some finishing touch to the landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
   <link href="{{ asset('frontend/assets/css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="{{ asset('frontend/assets/css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
-<body>
+<body style="font-size: 1.1em;">
   <div class="">
   <nav class="white" role="navigation">
     <div class="nav-wrapper container">
@@ -52,12 +52,12 @@
             @if (Route::has('login'))
       <ul id="mobile-demo" class="side-nav">
                     @auth
-        <li><a class="red" href="{{ url('/dashboard') }}">Dashboard</a></li>
+        <li><a class="red-text" href="{{ url('/dashboard') }}">Dashboard</a></li>
                     @else
-        <li><a class="red" href="{{ route('login') }}">Log in</a></li>
+        <li><a class="red-text" href="{{ route('login') }}">Log in</a></li>
 
                         @if (Route::has('register'))
-        <li><a class="red" href="{{ route('register') }}">Register</a></li>
+        <li><a class="red-text" href="{{ route('register') }}">Register</a></li>
                         @endif
                     @endauth
       </ul>
@@ -73,7 +73,7 @@
         <br><br>
         <h1 class="header center white-text text-lighten-2">Recordia</h1>
         <div class="row center">
-          <h5 class="header col s12 light">Financial recording for transparency</h5>
+          <h5 class="header col s12">Record diary for event donations</h5>
         </div>
         <div class="row center">
           <a href="{{ route('register') }}" id="download-button" class="btn-large waves-effect waves-light red accent-3 lighten-1">Get Started</a>
@@ -88,39 +88,7 @@
  <div class="container">
     <div class="section">
 
-      <div class="row">
-        <div class="col s12 center">
-          <h3><i class="mdi-content-send red-text"></i></h3>
-          <h4>About Recordia</h4>
-          <p class="left-align">Recordia is a portfolio project for <a class="red-text" href="https://www.holbertontulsa.com/">Holberton School</a> which is meant for financial secretaries who want to give almost instant financial reports to the organization they work with and give instant invoice to the donors. I got the inspiration for this app based on my experience with a non-profit organization that does launching and events for the purpose of raising funds. </p>
-          <p class="left-align">During the launching, donors are allowed to buy items(launching items) and proper records are taken for all launching and items purchased. Some pay in cash, some transfers while some pledges their support. At the end of the event, partakers will have their invoice prepared to know the summation of how much was paid and amount pledged for them to reconcile.</p>
-          <p class="left-align">The main challenge is that the process of getting the invoice prepared has always been hectic and prone to errors. Recordia is meant to automate the process of invoice preparation, getting it ready on request to be sent to the participant’s email or preferably their whatsapp numbers in a pdf format thereby ensuring a transparent invoice preparation. And also, give an on sight analysis of the total amount raised which will be the amount in cash, amount in transfer and total amount pledged waiting for reconciliation.</p>
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-
-
-  <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
-          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-        </div>
-      </div>
-    </div>
-    <div class="parallax"><img src="{{ asset('frontend/assets/background2.jpg') }}" alt="Unsplashed background img 2"></div>
-  </div>
-
- 
-
-  <div class="container">
-    <div class="section">
-
-      <!--   Icon Section   -->
-      <div class="row">
+      <div id="about" class="row">
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center red-text"><i class="material-icons">flash_on</i></h2>
@@ -135,7 +103,7 @@
             <h2 class="center red-text"><i class="material-icons">group</i></h2>
             <h5 class="center">User Experience Focused</h5>
 
-            <p class="center">Grants permission to users based on their varoius roles</p>
+            <p class="center">Grants permission to users based on their various roles</p>
           </div>
         </div>
 
@@ -148,51 +116,117 @@
           </div>
         </div>
       </div>
+      <div class="divider"></div>
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send red-text"></i></h3>
+          <h4>About Recordia</h4>
+          <p class="left-align">Recordia is a portfolio project for <a class="red-text" href="https://www.holbertontulsa.com/">Holberton School</a> which is meant for financial secretaries who want to give almost instant financial reports to the organization they work with and give instant invoice to the donors. I got the inspiration for this app based on my experience with a non-profit organization that does launching and events for the purpose of raising funds. </p>
+          <p class="left-align">During the launching, donors are allowed to buy items(launching items) and proper records are taken for all launching and items purchased. Some pay in cash, some transfers while some pledges their support. At the end of the event, partakers will have their invoice prepared to know the summation of how much was paid and amount pledged for them to reconcile.</p>
+          
+        </div>
+      </div>
 
     </div>
   </div>
+
 
 
   <div class="parallax-container valign-wrapper">
     <div class="section no-pad-bot">
       <div class="container">
         <div class="row center">
-          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+          <h5 class="header col s12 light">Remove the hazzle of manual calculation with calculator and paper</h5>
         </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="{{ asset('frontend/assets/background2.jpg') }}" alt="Unsplashed background img 2"></div>
+  </div>
+
+ 
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m6">
+          <img src="{{ asset('frontend/assets/laptop.png') }}" style="height: 25em;" >
+        </div>
+
+        <div class="col s12 m6">
+          <p class="left-align" style="margin-top:5em"><b>The main challenge is that the process of getting the invoice prepared has always been hectic and prone to errors.</b> Recordia is meant to automate the process of invoice preparation, getting it ready on request to be sent to the participant’s email or preferably their whatsapp numbers in a pdf format thereby ensuring a transparent invoice preparation. And also, give an on sight analysis of the total amount raised which will be the amount in cash, amount in transfer and total amount pledged waiting for reconciliation.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div id="features" class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        
+    <div class="col s12 card-content-link">
+      <div class="card z-depth-0 card-border-gray pb-3">
+        <div class="card-content">
+          <div class="row">
+            <div class="col s12">
+              <h5 class="black-text"><i class="material-icons red-text vertical-text-bottom"> grade </i> Features</h5>
+              <br>
+            </div>
+            <div class="col s12 m6">
+              <p class="grey-text">Automate the process of invoice preparation</p>
+              <div class="divider"></div>
+              <p class="grey-text">On sight analysis of the total amount raised</p>
+              <div class="divider"></div>
+              <p class="grey-text">Personalized interface for donors</p>
+            </div>
+            <div class="col s12 m6">
+              <p class="grey-text">Control flexibilty in role permission</p>
+              <div class="divider"></div>
+              <p class="grey-text">Generatd invoice received on email/whatsapp</p>
+              <div class="divider"></div>
+              <p class="grey-text">Daily, weekly, monthly and Annual financial statement</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
       </div>
     </div>
     <div class="parallax"><img src="{{ asset('frontend/assets/background3.jpg') }}" alt="Unsplashed background img 3"></div>
   </div>
 
-  <footer class="page-footer grey darken-2">
+  <footer class="page-footer grey darken-3">
     <div class="container">
-      <div class="row">
+      <div id="developer" class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Developer's Bio</h5>
-          <p class="grey-text text-lighten-4">Peter Adetola is a fullstack software engineer of alx software engineering program working on this project and this project is still a work in progress, stay tuned for updates.</p>
+          <h5 class="grey-text lighten-1" style="font-size:1.2em">About the Developer</h5>
+          <p class="grey-text text-lighten-4 light">Peter Adetola is a fullstack software engineer of alx software engineering program working on this project and this project is still a work in progress, stay tuned for updates.</p>
 
 
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Navigator</h5>
+          <h5 class="grey-text lighten-1" style="font-size:1.2em">Navigator</h5>
           <ul>
-            <li><a class="white-text" href="#!">About</a></li>
-            <li><a class="white-text" href="#!">Features</a></li>
-            <li><a class="white-text" href="#!">Developer</a></li>
+            <li><a class="white-text light" href="#!">About</a></li>
+            <li><a class="white-text light" href="#!">Features</a></li>
+            <li><a class="white-text light" href="#!">Developer</a></li>
           </ul>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">User Access</h5>
+          <h5 class="grey-text lighten-1" style="font-size:1.2em">User Access</h5>
      
           <ul>
             @if (Route::has('login'))
                     @auth
-            <li><a class="white-text" href="#!">Dashboard</a></li>
+            <li><a class="white-text" href="{{ route('/dashboard') }}">Dashboard</a></li>
                     @else
-            <li><a class="white-text" href="#!">Log in</a></li>
+            <li><a class="white-text" href="{{ route('login') }}">Log in</a></li>
 
                         @if (Route::has('register'))
-            <li><a class="white-text" href="#!">Register</a></li>
+            <li><a class="white-text" href="{{ route('register') }}">Register</a></li>
                         @endif
                     @endauth
             @endif
@@ -200,9 +234,9 @@
         </div>
       </div>
     </div>
-    <div class="footer-copyright grey darken-3">
+    <div class="footer-copyright grey darken-4">
       <div class="container">
-      Made by <a class="grey-text text-lighten-3" href="http://github.com/PeterAdetola">Peter Adetola</a>
+      Made by <a class="grey-text text-lighten-1" href="http://github.com/PeterAdetola">Peter Adetola</a>
       </div>
     </div>
   </footer>
